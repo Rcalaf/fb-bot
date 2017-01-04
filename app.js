bf3a773d42544995d01d6fff4fde82e8cc21198b
Roger Calaf <rogerc@BR-ROGERC.local>
2017-01-04 17:14:26 +0100

    log validation token check

--- app.js
+++ app.js
@@ -62,10 +62,12 @@ if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
  *
  */
 app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
 	console.log(req.query['hub.verify_token']);
 	console.log(VALIDATION_TOKEN);
+	console.log(req.query['hub.mode']);
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+	
     console.log("Validating webhook");
     res.status(200).send(req.query['hub.challenge']);
   } else {
