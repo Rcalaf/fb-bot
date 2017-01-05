206226853ffe62399881cb08ddce9fe86d9fa0b7
Roger Calaf <rogerc@BR-ROGERC.local>
2017-01-05 14:51:12 +0100

    greeting text

--- app.js
+++ app.js
@@ -825,26 +825,17 @@ function sendGreetings(recipientId) {
       qs: { access_token: PAGE_ACCESS_TOKEN },
       method: 'POST',
       json: {
-  			"setting_type":"call_to_actions",
-  			"thread_state":"new_thread",
-  			"call_to_actions":[
-    			{
-      			  "payload":USER_DEFINED_PAYLOAD
-    			}
-  		  	]
+  			"setting_type":"greeting",
+		    "greeting":{
+		      "text":USER_DEFINED_PAYLOAD
+		    }
 	  	}
     }, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        var recipientId = body.recipient_id;
-        var messageId = body.message_id;
-
-        if (messageId) {
-          console.log("Successfully sent message with id %s to recipient %s", 
-            messageId, recipientId);
-        } else {
+        
         console.log("Successfully called Send API for recipient %s", 
           recipientId);
-        }
+        
       } else {
         console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
       }
