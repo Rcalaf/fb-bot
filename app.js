39d1e60b2aa1235b046a0ea143c64799666bcc70
Roger Calaf <rogerc@BR-ROGERC.local>
2017-01-11 16:28:10 +0100

    check user data

--- app.js
+++ app.js
@@ -145,9 +145,12 @@ function sendUserProfileApi(messagingEvent) {
 			  	state: "greeting", 
 				name: body.first_name
 	  	  	}
-			sendMessage2(messagingEvent.sender.id,messagesTypes[botData["state"]][botData["attempts"]]);
+			console.log("body: " + JSON.stringify(body));
+			console.log("messages: " + JSON.stringify(messagesTypes));
+			console.log("data: " + JSON.stringify(botData));
+			sendMessage2(messagingEvent.sender.id,messagesTypes[botData[messagingEvent.sender.id].state][botData[messagingEvent.sender.id].attempts);
 			//sendTextMessage(messagingEvent.sender.id, "Hola "+botData[messagingEvent.sender.id].name+"! como estas?");
-   		    console.log("Successfully called Send API " + JSON.stringify(botData));
+   		    console.log("Successfully called Send API " + JSON.stringify(body));
       } else {
         console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
       }
