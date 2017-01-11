a672356f983ee0111adb50d9c2a92d1467064de5
Roger Calaf <rogerc@BR-ROGERC.local>
2017-01-11 12:34:55 +0100

    back to default

--- app.js
+++ app.js
@@ -17,12 +17,16 @@ const
   express = require('express'),
   https = require('https'),  
   request = require('request');
+  //mongodb = require('mongodb');
 
 var app = express();
 app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.use(express.static('public'));
+
+var botData = {};
+
 
 /*
  * Be sure to setup your config values before running this code. You can 
