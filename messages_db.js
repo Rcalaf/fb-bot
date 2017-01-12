baa7770b818c3d7ef9370f25467067e70cf425fc
Roger Calaf <rogerc@BR-ROGERC.local>
2017-01-12 17:30:18 +0100

    check user data

--- messages_db.js
+++ messages_db.js
@@ -1,10 +1,4 @@
-const config = require('config');
 
-// URL where the app is running (include protocol). Used to point to scripts and 
-// assets located at this address. 
-const SERVER_URL = (process.env.SERVER_URL) ?
-  (process.env.SERVER_URL) :
-  config.get('serverURL');
 
 exports.messagesTypes = {
 	saludos: {
