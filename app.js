f9548b94d1ae9236f45e629b35274d8c1e895fa0
Roger Calaf <rogerc@BR-ROGERC.local>
2017-01-17 13:09:30 +0100

    added and updated llantas

--- app.js
+++ app.js
@@ -522,8 +522,6 @@ function receivedPostback(event) {
 		botData[senderID].attempts = 0;
 		sendMessage2(senderID, messagesTypes[botData[senderID].state][0]);
 	}else if(payload == "FACEBOOK_WELCOME"){
-		botData[senderID].state = "saludos";
-		botData[senderID].attempts = 0;
 		sendUserProfileApi(messagingEvent);
 	}
  
